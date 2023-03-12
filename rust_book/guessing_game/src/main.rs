4041be6caa177c44590d1c227a02d7ce3132bfe9
Ezdies <bvbclubbvb@gmail.com>
2023-03-12 14:35:01 +0100

    Added looping and error handling

--- rust_book/guessing_game/src/main.rs
+++ rust_book/guessing_game/src/main.rs
@@ -9,21 +9,29 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess");
+    loop {
+        println!("Please input your guess");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed tp read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed tp read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You won!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You won!");
+                break;
+            }
+        }
     }
 }
