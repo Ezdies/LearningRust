2efa5fdf52658c9009736e48a888d23d34e8e4e8
Ezdies <bvbclubbvb@gmail.com>
2023-03-28 09:16:09 +0200

    Added new function to the ex3

--- uni/labs/lab5/ex1/src/main.rs
+++ uni/labs/lab5/ex1/src/main.rs
@@ -20,16 +20,16 @@ fn co_drugi_znak(napis: &str) -> String {
     res
 }
 
-fn odwrocony_kawalek(s: &str, beg: usize, en: usize){
-    match str {
-        be => {},
-    }
+fn odwrocony_kawalek(s: &str, beginning_index: usize, end_index: usize) -> String {
+    let my_fragment = &s[beginning_index..=end_index];
+    let reversed_fragment = my_fragment.chars().rev().collect();
 
+    reversed_fragment
 }
 
-fn szyfruj(napis: &str, klucz: u32) -> String {
+//fn szyfruj(napis: &str, klucz: u32) -> String {
 
-}
+//}
 
 fn main() {
     let res1 = liczba_wystapien("hello world", 'o');
@@ -37,4 +37,7 @@ fn main() {
 
     let res2 = co_drugi_znak("hello world");
     println!("Co drugi znak: {res2}");
+
+    let res3 = odwrocony_kawalek("siema", 0, 4);
+    println!("Odwrócony kawałek siema to: {res3}");
 }
