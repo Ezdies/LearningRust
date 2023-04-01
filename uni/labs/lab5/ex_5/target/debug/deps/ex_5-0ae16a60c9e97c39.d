a926d392c9f070868b26fa92f167c9d32d683654
Ezdies <bvbclubbvb@gmail.com>
2023-04-01 14:32:04 +0200

    Started ex_5

---
+++ uni/labs/lab5/ex_5/target/debug/deps/ex_5-0ae16a60c9e97c39.d
@@ -0,0 +1,5 @@
+/home/ezdies/Rust/uni/labs/lab5/ex_5/target/debug/deps/ex_5-0ae16a60c9e97c39: src/main.rs
+
+/home/ezdies/Rust/uni/labs/lab5/ex_5/target/debug/deps/ex_5-0ae16a60c9e97c39.d: src/main.rs
+
+src/main.rs:
---
+++ uni/labs/lab5/ex_5/target/debug/ex_5.d
@@ -0,0 +1,1 @@
+/home/ezdies/Rust/uni/labs/lab5/ex_5/target/debug/ex_5: /home/ezdies/Rust/uni/labs/lab5/ex_5/src/main.rs
