f681471dd587f394442d0105d46ddae78d9bf072
Ezdies <bvbclubbvb@gmail.com>
2023-04-01 17:05:18 +0200

    Changed comparing string to char

--- uni/labs/lab5/ex_5/src/main.rs
+++ uni/labs/lab5/ex_5/src/main.rs
@@ -1,16 +1,16 @@
 fn roman_to_arabic(roman_literal: &str) -> Option<u32> {
     let numerals = vec![
-        ("I", 1),
-        ("V", 5),
-        ("X", 10),
-        ("L", 50),
-        ("C", 100),
-        ("D", 500),
-        ("M", 1000),
+        ('I', 1),
+        ('V', 5),
+        ('X', 10),
+        ('L', 50),
+        ('C', 100),
+        ('D', 500),
+        ('M', 1000),
     ];
 
     for (numeral, value) in &numerals {
-        if *numeral == roman_literal {
+        if *numeral == roman_literal.chars().next().unwrap() {
             return Some(*value);
         }
     }
